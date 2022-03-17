4b2d907ed581d7282ff3b6828ef4ac4920ef43b7
Jack <iatenine@gmail.com>
2022-03-17 14:40:23 -0600

    Basic implementation of Display

--- src/features/display/Display.tsx
+++ src/features/display/Display.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-type Props = {};
+interface Squak {
+  id: number;
+  content: string;
+  date: number;
+}
 
-// Component to display all posts
+const Display = () => {
+  // Use Axios to get all posts from the database
+  const [posts, setPosts] = useState<Squak[]>([]);
 
-const Display = (props: Props) => {
-  return <div>display</div>;
+  useEffect(() => {
+    axios
+      .get("https://squakbox-app.azurewebsites.net/", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => {
+        setPosts(res.data);
+      });
+  }, []);
+
+  const getDateFromTimestamp = (timestamp: number) => {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  };
+
+  const postList = posts.map((post) => (
+    <div key={post.id}>
+      <h3>{getDateFromTimestamp(post.date) + ": " + post.content}</h3>
+    </div>
+  ));
+
+  return <div>{postList}</div>;
 };
 
 export default Display;
