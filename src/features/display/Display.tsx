7011d19d8ce6fcef896a843a555193e3b6ef16a7
Jack <iatenine@gmail.com>
2022-03-17 15:15:12 -0600

    Squaks refresh accordingly now

--- src/features/display/Display.tsx
+++ src/features/display/Display.tsx
@@ -7,28 +7,15 @@ interface Squak {
   date: number;
 }
 
-const Display = () => {
+const Display = (props: { posts: Squak[] }) => {
   // Use Axios to get all posts from the database
-  const [posts, setPosts] = useState<Squak[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("https://squakbox-app.azurewebsites.net/", {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      })
-      .then((res) => {
-        setPosts(res.data);
-      });
-  }, []);
 
   const getDateFromTimestamp = (timestamp: number) => {
     const date = new Date(timestamp);
     return date.toLocaleString();
   };
 
-  const postList = posts.map((post) => (
+  const postList = props.posts.map((post: Squak) => (
     <div key={post.id}>
       <h3>{getDateFromTimestamp(post.date) + ": " + post.content}</h3>
     </div>
--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,12 +1,73 @@
-import React from "react";
-import { JsxElement } from "typescript";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Display from "../display/Display";
 
-type Props = {};
+interface Squak {
+  id: number;
+  content: string;
+  date: number;
+}
 
-// Component for users to input new post to be created
+const Post = () => {
+  const [squakInput, setSquakInput] = useState<string>("");
+  const [valid, setValid] = useState(false);
+  const [posts, setPosts] = useState<Squak[]>([]);
 
-const Post = (props: Props) => {
-  return <>Post</>;
+  const updatePosts = () => {
+    axios
+      .get("https://squakbox-app.azurewebsites.net/", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // Update the posts every 20 seconds
+  useEffect(() => {
+    updatePosts();
+    const interval = setInterval(() => {
+      updatePosts();
+    }, 20000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const onHandleChange = (e: {
+    target: { value: any | React.SetStateAction<string> };
+  }) => {
+    setSquakInput(e.target.value);
+    setValid(e.target.value.length > 0 && e.target.value.length < 255);
+  };
+
+  const onHandleSubmit = () => {
+    if (squakInput.length == 0 || squakInput.length > 255) return;
+    axios
+      .post("https://squakbox-app.azurewebsites.net/", {
+        content: squakInput,
+      })
+      .then((res) => {
+        setSquakInput("");
+        updatePosts();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <>
+      <textarea value={squakInput} onChange={onHandleChange} />
+      <button onClick={onHandleSubmit} disabled={!valid}>
+        Submit
+      </button>
+      <Display posts={posts} />
+    </>
+  );
 };
 
 export default Post;
