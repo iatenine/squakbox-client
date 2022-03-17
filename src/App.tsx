c54374eb853840d838329eaf7582ab7de1849643
Jack <iatenine@gmail.com>
2022-03-17 15:25:18 -0600

    Correct Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Display from "./features/display/Display";
 import Post from "./features/post/Post";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "Squakbox";
+  }, []);
+
   return (
     <>
       <Post />
